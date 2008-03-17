ce8ab304d4a57225fcb77c04896b33806c43fcf3
mbyhring <mbyhring@5981d7da-5c43-0410-b447-d1179332cb14>
2008-03-17 12:36:28 +0000

    Har lagt til noen metoder: getDistinctLocation -- henter alle unike lokasjoner fra inv getNodesWithLocation	-- returnerer en hash med maskinnavn og tilhørende lokasjon
    Disse to er egentlig bare en kopi av de andre db-metodene våre, kun endret query-stringen.
    Derfor bør det være mulig å lage noen generiske db-metoder som kun tar imot tabellnavn og felter.
    Har kommentert ut mysql-basen på cube fordi jeg kjører lokalt nå.

---  hovedprosjekt08 --username mbyhring/lcfgVisualizer/lib/DBMETODER.pm
+++  hovedprosjekt08 --username mbyhring/lcfgVisualizer/lib/DBMETODER.pm
@@ -11,19 +11,25 @@ use Exporter ();
 our @ISA         = qw( Exporter );
 
 # Here we define which methods are gonna be exported
-our @EXPORT      = qw( getHashGateways getArrDistinct getNodesWithOS );
+our @EXPORT      = qw( getHashGateways getArrDistinct getNodesWithOS getNodesWithLocation getDistinctLocation );
 
-#Definitions for the connectionvariables:
-my $db = "s134850";
-my $host = "cube.iu.hio.no";
-my $user = "s134850";
-my $password = "passord";
+#Definitions for the connectionvariables: (Mortens laptop)
+my $db = "hovedpro";
+my $host = "localhost";
+my $user = "hovedpro";
+my $password = "morten!";
+
+#These are the old values, uncomment these to use:
+#my $database = "s134850";
+#my $host = "cube.iu.hio.no";
+#my $user = "s134850";
+#my $pass = "passord";
 
 
 sub getNodesWithOS
 {
-	my $table = "inv2";
-	
+	my $table = "inv";
+	#my $table = "inv2"; #Uncomment this if your table name is inv2..
 	my $query = "select machinename, os from $table";
 	my $dbh = DBI->connect("DBI:mysql:database=$db:host=$host",
 			$user,
@@ -43,6 +49,10 @@ sub getNodesWithOS
 		{
 			$machines{$hostid} = $os;
 			#print "Hostid: " . $hostid . " gateway: " . $gateway . "\n";
+		}
+		else
+		{
+			$machines{$hostid} = "unknown";
 		}
 	}
 
@@ -105,6 +115,74 @@ sub getArrDistinct
 	#print "\n\nSuccessfully connected to " . $host . " to database " . $db . "!\n\n";
 
 	my $sql = qq{ SELECT DISTINCT gateway FROM network };
+	my $sth = $dbh->prepare($sql);
+	$sth->execute();
+
+	my ( $gateway );
+	$sth->bind_columns( undef, \$gateway );
+
+	my @gatewayDistinct = ();
+
+	while ( $sth->fetch() )
+	{
+		if($gateway)
+		{
+			push(@gatewayDistinct,$gateway);
+		}
+	}
+
+	$dbh->disconnect;
+	return @gatewayDistinct;
+}
+
+sub getNodesWithLocation
+{
+	my $table = "inv";
+	
+	my $query = "select machinename, location from $table";
+	my $dbh = DBI->connect("DBI:mysql:database=$db:host=$host",
+			$user,
+			$password)
+			or die DBI::errstr;
+	my %machines;
+	my $sql = qq{$query};	
+	my $sth = $dbh->prepare($sql);
+	
+	$sth->execute();
+	my ( $hostid , $loc );
+	$sth->bind_columns( undef, \$hostid, \$loc );
+
+	while ($sth->fetch())
+	{
+		if ($loc)
+		{
+			$machines{$hostid} = $loc;
+			#print "Hostid: " . $hostid . " gateway: " . $gateway . "\n";
+		}
+		else
+		{
+			$machines{$hostid} = "unknown";
+		}
+	}
+
+	$sth->finish;
+
+	#Close the connection
+	$dbh->disconnect;
+
+	return %machines;
+}
+
+sub getDistinctLocation
+{
+	#Opens new connection
+	my $dbh = DBI->connect("DBI:mysql:database=$db:host=$host",
+			$user,
+			$password)
+			or die DBI::errstr;
+	#print "\n\nSuccessfully connected to " . $host . " to database " . $db . "!\n\n";
+
+	my $sql = qq{ SELECT DISTINCT location FROM inv };
 	my $sth = $dbh->prepare($sql);
 	$sth->execute();
 
