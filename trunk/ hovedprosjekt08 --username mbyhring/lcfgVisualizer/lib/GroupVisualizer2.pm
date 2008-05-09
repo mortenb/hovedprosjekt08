0d8545045e195abb32aa52881ca9d77f382fef35
t.e.iversen <t.e.iversen@5981d7da-5c43-0410-b447-d1179332cb14>
2008-05-09 11:38:53 +0000

    This is RC1

--- trunk/ hovedprosjekt08 --username mbyhring/lcfgVisualizer/lib/GroupVisualizer2.pm
+++ trunk/ hovedprosjekt08 --username mbyhring/lcfgVisualizer/lib/GroupVisualizer2.pm
@@ -7,8 +7,6 @@ use POSIX qw(ceil );
 use DAL;  #Data Access Layer, connects to DB
 use VRML_Generator; 
 
-#my $cfgFilePath = "cfg/vcsd.cfg";
-
 my @paramsCriteria1;
 my @paramsCriteria2;
 my @paramsCriteria3;
@@ -50,257 +48,174 @@ my $vrmlGen = VRML_Generator->new();
 
 sub generateWorld()
 {
-%crit1 = $dal->getNodesWithCriteriaHash(@paramsCriteria1);
-%crit2 = $dal->getNodesWithCriteriaHash(@paramsCriteria2);
-%allNodesInfo = $dal->getAllNodesInformation();
-
-foreach my $key (keys %crit2)
-{
-	if(!exists $crit1{$key})	
+	%crit1 = $dal->getNodesWithCriteriaHash(@paramsCriteria1);
+	%crit2 = $dal->getNodesWithCriteriaHash(@paramsCriteria2);
+	%allNodesInfo = $dal->getAllNodesInformation();
+	
+	foreach my $key (keys %crit2)
 	{
-		$crit1{$key} = "undefined";
+		if(!exists $crit1{$key})	
+		{
+			$crit1{$key} = "undefined";
+		}
 	}
+	
+	if(@paramsCriteria3 > 2)  #funker ikke helt.. Får feilmeldinger i terminal hvis man ikke legger ved nok parametere, men alt genereres ok likevel.
+	{
+		%crit3 = $dal->getNodesWithChosenCriteria(@paramsCriteria3);
+	}
+	#Get the distinct criteria values by reversing the hash:
+	my %distinctCrit2 = reverse %crit2;
+	my @arr = keys  %distinctCrit2;
+	my $menuItems = &makeDefNodes();
+	
+	#Print the vrml file starting with the header
+	$vrmlString .= $vrmlGen->header(); 
+	# required proto definitions
+	$vrmlString .= $vrmlGen->vrmlViewChangeProtoDef();
+	$vrmlString .= $vrmlGen->vrmlNodeProtoDef();
+	$vrmlString .= $vrmlGen->vrmlMenuItemProtoDef();
+	# a timer needed for animation
+	$vrmlString .= $vrmlGen->timer("timer", 4, "FALSE");
+	# The world geometry and HUD
+	$vrmlString .= $vrmlGen->startVrmlGroup("TheWorld");
+	
+	$vrmlString .= $vrmlGen->vrmlHUD($menuItems, 10000, 10000, 10000);
+	# Grouping nodes
+	$vrmlString .= $vrmlGen->criteria2NodesAnchorNavi(@arr);
+	# The nodes representing hosts
+	$vrmlString .= makeNodes();
+	# end of world geometry
+	$vrmlString .= $vrmlGen->endVrmlGroup();
+	# Add routes for animation and node information for all nodes
+	$vrmlString .= "\n#Routes for node information and animation:\n";
+	foreach my $key ( keys %crit1)
+	{
+		my $safeNodeName = $vrmlGen->returnSafeVrmlString($key);
+		$vrmlString .= "ROUTE $safeNodeName.nodeDesc TO nodeinfoText.set_info\n";
+		$vrmlString .= "ROUTE timer.fraction_changed TO $safeNodeName.set_fraction\n";
+	}
+	
+	#print the rest of the routes.
+	$vrmlString .= $vrmlRoutes;
+	$vrmlString .= $vrmlGen->printRoutes();
+		
+	#return the generated vrml
+	return $vrmlString;	
 }
 
-if(@paramsCriteria3 > 2)  #funker ikke helt.. Får feilmeldinger i terminal hvis man ikke legger ved nok parametere, men alt genereres ok likevel.
-{
-	%crit3 = $dal->getNodesWithChosenCriteria(@paramsCriteria3);
-}
-#Get the distinct criteria values by reversing the hash:
-my %distinctCrit2 = reverse %crit2;
-my @arr = keys  %distinctCrit2;
-my $menuItems = &makeDefNodes();
-
-#Print the vrml file
-$vrmlString .= $vrmlGen->header();
-$vrmlString .= $vrmlGen->vrmlViewChangeProtoDef();
-$vrmlString .= $vrmlGen->vrmlNodeProtoDef();
-$vrmlString .= $vrmlGen->vrmlMenuItemProtoDef();
-$vrmlString .= $vrmlGen->timer("timer", 4, "FALSE");
-$vrmlString .= $vrmlGen->startVrmlGroup("TheWorld");
-
-$vrmlString .= $vrmlGen->vrmlHUD($menuItems, 10000, 10000, 10000);
-$vrmlString .= $vrmlGen->criteria2NodesAnchorNavi(@arr);
-$vrmlString .= makeNodes();
-$vrmlString .= $vrmlGen->endVrmlGroup();
-# Add routes for animation and node information for all nodes
-$vrmlString .= "\n#Routes for node information and animation:\n";
-foreach my $key ( keys %crit1)
-{
-	my $safeNodeName = $vrmlGen->returnSafeVrmlString($key);
-	$vrmlString .= "ROUTE $safeNodeName.nodeDesc TO nodeinfoText.set_info\n";
-	$vrmlString .= "ROUTE timer.fraction_changed TO $safeNodeName.set_fraction\n";
-}
-
-$vrmlString .= $vrmlRoutes;
-$vrmlString .= $vrmlGen->printRoutes();
-
-#print the rest of the routes and a start button..
-
-return $vrmlString;
-
-}
-
-
+# Creates vrml definitions for all the criteria1 nodes
 sub makeDefNodes()
 {
-	#this method takes care of setting criteia1-values to a colour
-	#TODO: generic colour generating?  
-#my @colors; # array with color definitions
-#
-#my $red = "material DEF RedColor Material {
-#				diffuseColor 1.0 0 0
-#			}";
-#
-#my $blue = "material DEF BlueColor Material {
-#				diffuseColor 0 0 1.0
-#			}";
-#my $yellow = "material DEF YellowColor Material {
-#				diffuseColor 1.0 1.0 0
-#			}";
-#
-#my $green = "material DEF GreenColor Material {
-#				diffuseColor 0 1.0 0
-#			}";
-#			
-#my $purple = "material DEF PurpleColor Material {
-#				diffuseColor 0.3 0 0.3
-#			}";
-#
-#my $pink = "material DEF PinkColor Material {
-#				diffuseColor 1 0 0.5
-#			}";
-#			
-#my $white = "material DEF WhiteColor Material {
-#				diffuseColor 1 1 1
-#			}";
-#			
-#my $mint = "material DEF MintColor Material {
-#				diffuseColor 0 1 1
-#			}";
-#			
-#my $orange = "material DEF OrangeColor Material {
-#				diffuseColor 1 0.5 0.1
-#			}";
-#			
-#my $color1 = "material DEF Color1 Material {
-#				diffuseColor 0.6 0.6 0.34
-#			}";
-#			
-#my $color2 = "material DEF Color2 Material {
-#				diffuseColor 0.3 0.6 0.3
-#			}";
-#
-#my $color3 = "material DEF Color3 Material {
-#				diffuseColor 0.7 0.7 0.4
-#			}";		
-#			
-#my $color4 = "material DEF Color4 Material {
-#				diffuseColor 0 0.6 0.36
-#			}";		
-#			
-#my $grey = 	"material DEF grey Material {
-#				diffuseColor 0.3 0.3 0.3
-#			}";			
-#	
-#	my $lightBlue = 	"material DEF lightBlue Material {
-#				diffuseColor 0.5 0.6 1
-#			}";	
-#			
-#my $lightRed ="material DEF lightRed Material {
-#				diffuseColor 1 0.5 0.5
-#			}";					
-#$colors[0] = $red;
-#$colors[1] = $blue;
-#$colors[2] = $yellow;
-#$colors[3] = $green;
-#$colors[4] = $purple;
-#$colors[5] = $pink;
-#$colors[6] = $white;
-#$colors[7] = $mint;
-#$colors[8] = $orange;
-#$colors[9] = $color1;
-#$colors[10] = $color2;
-#$colors[11] = $color3;
-#$colors[12] = $color4;
-#$colors[13] = $grey;	
-#$colors[14] = $lightBlue;
-#$colors[15] = $lightRed;
-#
-#			
-my %distinctCrit1 = reverse %crit1;
-#my $counter = 0;
-#foreach my $key ( keys %distinctCrit1 )
-#{
-#	$distinctCrit1{$key} = $colors[$counter++];
-#	#Assign every distinct criteria1, a spesific colour.
-#}
-
-my $string = $vrmlGen->vrmlDefNodesV3(%distinctCrit1);
-return $string;
+	my %distinctCrit1 = reverse %crit1;
+	
+	my $string = $vrmlGen->vrmlDefNodesV3(%distinctCrit1);
+	return $string;
 }
 ###
 
 sub makeNodes()
 {
-my $vrmlString=""; #locale string
-my @keys = sort { $crit1{$a} cmp $crit1{$b} } keys %crit1;
-#@keys are nodenames ordered by criteria1-value
-
-foreach my $key ( @keys )
-{
-	#Run through the nodes and see if they have an entry in the crit2-hash
-	my $nodeName = $key;
-	my $tmp1 = $crit1{$key};
-	my $tmp2; 
-	if(!exists ($crit2{$key}))
+	my $vrmlString=""; #locale string
+	my @keys = sort { $crit1{$a} cmp $crit1{$b} } keys %crit1;
+	#@keys are nodenames ordered by criteria1-value
+	
+	foreach my $key ( @keys )
 	{
-		$tmp2 = "unknown"; # If they don't, they are "unknown"
-	}                        
-	else
-	{
-		$tmp2 = $crit2{$key}; #Get the nodes criteria2-value
-	}
-	$machines{$tmp1}{$nodeName} = $tmp2;  #Insert as a hash 
-}
-my $innerCounter = 0;
-my $prevCrit2Group ="";
-my $currCrit2Group ="";
-my @routeNames;
-
-foreach my $key ( keys %machines) #Run through all the collected nested data
-{
-	$currCrit2Group = "";  #reset the criterias and counter
-	$prevCrit2Group = "";
-	$innerCounter =0;
-
-	$vrmlString .= $vrmlGen->startVrmlGroup("group_crit1_eq_".$key); #start a mother group
-	#Foreach criteria1, sort by criteria2.
-	foreach my $key2 ( sort  { $machines{$key}{$a} cmp $machines{$key}{$b} } keys %{$machines{$key}} )
-	{
-
-############ TESTCODE Nodeinfo ############
-#TODO:  write better code for   nodeinfo  #
-###########################################
-		
-		my $nodeinfo = "\"Hostname: $key2\"";
-		my $testref = \%allNodesInfo;
-		foreach my $testkey(keys %{$testref->{$key2}})
+		#Run through the nodes and see if they have an entry in the crit2-hash
+		my $nodeName = $key;
+		my $tmp1 = $crit1{$key};
+		my $tmp2; 
+		if(!exists ($crit2{$key}))
 		{
-			foreach my $testkey2(keys %{$testref->{$key2}->{$testkey}})
-			{
-				my $value = $testref->{$key2}->{$testkey}->{$testkey2};
-				$nodeinfo .= ", \"$testkey2: $value\"";
-			}
+			$tmp2 = "unknown"; # If they don't, they are "unknown"
+		}                        
+		else
+		{
+			$tmp2 = $crit2{$key}; #Get the nodes criteria2-value
 		}
-		
-		
-###########################################
-		my $safeNodeName = $vrmlGen->returnSafeVrmlString($key2);
-
-		#Determine if node satisfies criteria 3. If so, set criteri3 = TRUE and add route for menu toggle
-		my $crit3= "FALSE";
-		if(exists( $crit3{"$key2"})) #Check if current machine fulfills criteria3
+		$machines{$tmp1}{$nodeName} = $tmp2;  #Insert as a hash 
+	}
+	my $innerCounter = 0;
+	my $prevCrit2Group ="";
+	my $currCrit2Group ="";
+	my @routeNames;
+	
+	foreach my $key ( keys %machines) #Run through all the collected nested data
+	{
+		$currCrit2Group = "";  #reset the criterias and counter
+		$prevCrit2Group = "";
+		$innerCounter =0;
+	
+		$vrmlString .= $vrmlGen->startVrmlGroup("group_crit1_eq_".$key); #start a mother group
+		#Foreach criteria1, sort by criteria2.
+		foreach my $key2 ( sort  { $machines{$key}{$a} cmp $machines{$key}{$b} } keys %{$machines{$key}} )
+		{
+	
+		# Generate a string containing node information for the HUD popup
+		# The format of the string will be interpreted as a MFString by the vrml browser
+		# Giving one line of output for each \"\" block in the string		
+			my $nodeinfo = "\"Hostname: $key2\"";
+			my $testref = \%allNodesInfo;
+			foreach my $testkey(keys %{$testref->{$key2}})
+			{
+				foreach my $testkey2(keys %{$testref->{$key2}->{$testkey}})
+				{
+					my $value = $testref->{$key2}->{$testkey}->{$testkey2};
+					$nodeinfo .= ", \"$testkey2: $value\"";
+				}
+			}
+	
+			# Create a safe nodename for the vrml DEF statement
+			my $safeNodeName = $vrmlGen->returnSafeVrmlString($key2);
+	
+			# Determine if node satisfies criteria 3. If so, set criteri3 = TRUE and add route for menu toggle
+			my $crit3= "FALSE";
+			if(exists( $crit3{"$key2"})) #Check if current machine fulfills criteria3
 			{
 				$crit3= "TRUE";
 				$vrmlRoutes .= "ROUTE menuItemCrit3.isActive TO $safeNodeName.set_criteria3\n";
 			}
-	
-		my @randomPos = $vrmlGen->randomPos();
-		
-		$currCrit2Group = $machines{$key}{$key2};
-		if($prevCrit2Group ne $currCrit2Group	)  #Check if this is the same
-		{
-			#if it is a new group, we must end the previous transform and start a new transform
-			if($innerCounter > 0) #don't end the previous group if this is the first child group
+			
+			# Create a random start position for a node
+			my @randomPos = $vrmlGen->randomPos();
+			
+			$currCrit2Group = $machines{$key}{$key2};
+			if($prevCrit2Group ne $currCrit2Group	)  #Check if this is the same
 			{
-				$vrmlString .= $vrmlGen->endVrmlTransform(0,0,0);
+				#if it is a new group, we must end the previous transform and start a new transform
+				if($innerCounter > 0) #don't end the previous group if this is the first child group
+				{
+					$vrmlString .= $vrmlGen->endVrmlTransform(0,0,0);
+				}
+				
+				# will be used to create routes for animation of the group of nodes
+				# We cannot print routes inside this structure so we save them for later.
+				push(@routeNames, $machines{$key}{$key2});
+				push(@routeNames, "group_crit1_eq_".$key."_and_crit2_eq_".$currCrit2Group);
+				
+				$vrmlString .= $vrmlGen->startVrmlTransform("group_crit1_eq_".$key."_and_crit2_eq_".$currCrit2Group); #Make a child group	
 			}
 			
-			
-			push(@routeNames, $machines{$key}{$key2}); #We cannot print routes inside this structure so we save them for later.
-			push(@routeNames, "group_crit1_eq_".$key."_and_crit2_eq_".$currCrit2Group); #Dette skal brukes til ruter for å få til animasjon
-			
-			$vrmlString .= $vrmlGen->startVrmlTransform("group_crit1_eq_".$key."_and_crit2_eq_".$currCrit2Group); #Make a child group
-			
-		}
-		my @endPosition = $vrmlGen->randomSphereCoords(15,30,2);
-		$vrmlString .= $vrmlGen->vrmlNodeProtoDeclaration( "$safeNodeName",$vrmlGen->vrmlMakeNode( $key), "$nodeinfo", "$randomPos[0] $randomPos[1] $randomPos[2]", $crit3, "@endPosition" );
-		$prevCrit2Group = $currCrit2Group;
-		$innerCounter++;
+			# Create a random end position for a node
+			my @endPosition = $vrmlGen->randomSphereCoords(15,30,2);
+			# Create the node
+			$vrmlString .= $vrmlGen->vrmlNodeProtoDeclaration( "$safeNodeName",$vrmlGen->vrmlMakeNode( $key), "$nodeinfo", "$randomPos[0] $randomPos[1] $randomPos[2]", $crit3, "@endPosition" );
+			$prevCrit2Group = $currCrit2Group;
+			$innerCounter++;		
+		} 
+		$vrmlString .= $vrmlGen->endVrmlTransform(0,0,0); #end the last transform and the whole group
+
+		$vrmlString .= $vrmlGen->endVrmlGroup();
+	}
 		
-	} 
-	$vrmlString .= $vrmlGen->endVrmlTransform(0,0,0); #end the last transform and the whole group
-	
-	$vrmlString .= $vrmlGen->endVrmlGroup();
-}	
 	#Now we can generate and print the routes needed for animation:
 	for (my $i = 0; $i < @routeNames;  $i++)
 	{
 		my $safeName = $vrmlGen->returnSafeVrmlString($routeNames[$i]);
 		$vrmlRoutes .= $vrmlGen->makeVrmlRoute("pi".$safeName, "value_changed", $routeNames[++$i], "translation");
 	}
-return $vrmlString;
+	return $vrmlString;
 }
 #end method makeNodes
 
